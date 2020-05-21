3323b8985318f0a13fd446d6679728df05ef9634
cleysonsk <clbbdev@gmail.com>
2020-05-20 21:25:33 -0300

    change library name

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-week-notes'
+import { ReactWeekNotes } from 'react-week-notes'
 import 'react-week-notes/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ReactWeekNotes text="Create React Library Example 😄" />
 }
 
 export default App
