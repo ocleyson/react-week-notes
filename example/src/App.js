d843a878926e391e7c3b45009c8ff5548f3a74c4
cleysonsk <clbbdev@gmail.com>
2020-05-21 20:56:54 -0300

    add props interface

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ReactWeekNotes } from 'react-week-notes'
 import 'react-week-notes/dist/index.css'
 
 const App = () => {
-  return <ReactWeekNotes text="Create React Library Example 😄" />
+  return <ReactWeekNotes notes={[{name: 'ingles', time: new Date(), day: 0}]} color="#0747a6" />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-interface Props {
-  text: string
+interface Notes {
+  [index: number]: {
+    name: string
+    time: Date
+    day: number
+  }
 }
 
-export const ReactWeekNotes = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+interface Props {
+  color: string
+  notes: Notes
+}
+
+const week = ['seg', 'ter', 'qua', 'qui', 'sex', 'sáb', 'dom']
+
+export const ReactWeekNotes = ({ color, notes }: Props) => {
+  return (
+    <div className={styles.test}>
+      <div>
+        <ul>
+          {week.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
