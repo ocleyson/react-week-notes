0a1dc22420c32195d18a50c027950c6f9d751048
cleysonsk <clbbdev@gmail.com>
2020-05-25 23:13:53 -0300

    change list to table

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ReactWeekNotes } from 'react-week-notes'
 import 'react-week-notes/dist/index.css'
 
 const App = () => {
-  return <ReactWeekNotes notes={[{name: 'ingles', time: new Date(), day: 0}]} color="#0747a6" />
+  return <ReactWeekNotes notes={[{name: 'ingles', time: new Date(), day: 0}, {name: 'portugues', time: new Date(), day: 1}, {name: 'matematica', time: new Date(), day: 3}, {name: 'matematica', time: new Date(), day: 3} ]} color="#6baade" />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,117 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.module.css'
 
 interface Notes {
-  [index: number]: {
-    name: string
-    time: Date
-    day: number
-  }
+  name: string
+  time: Date
+  day: number
 }
 
 interface Props {
-  color: string
-  notes: Notes
+  color?: string
+  notes: Notes[]
 }
 
-const week = ['seg', 'ter', 'qua', 'qui', 'sex', 'sáb', 'dom']
+const week = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb']
+
+const today = new Date().getDay()
 
 export const ReactWeekNotes = ({ color, notes }: Props) => {
+  const [allNotes, setAllNotes] = useState<any>([])
+
+  useEffect(() => {
+    var reducedNotes = notes.reduce((allNotes: Array<any>, note: Notes) => {
+      if (allNotes.some((item) => item.time.getTime() === note.time.getTime()))
+        return allNotes
+
+      var notesFilteredByDate = notes.filter((item: Notes) => {
+        return item.time.getTime() === note.time.getTime()
+      })
+
+      var numberOfColums = getNumberOfColums(notesFilteredByDate)
+
+      allNotes.push({
+        time: note.time,
+        notes: notesFilteredByDate,
+        numberOfColums
+      })
+
+      return allNotes
+    }, [])
+
+    var reducedNotesSortedByDate = reducedNotes.sort((a, b) => a.time - b.time)
+
+    setAllNotes(reducedNotesSortedByDate)
+  }, [])
+
+  function getNumberOfColums(notes: any) {
+    var number = 0
+
+    for (var i = 0; i <= week.length; i++) {
+      var numberOfCols = notes.filter((item: Notes) => item.day === i).length
+
+      if (number < numberOfCols) {
+        number = numberOfCols
+      }
+    }
+
+    return number
+  }
+
   return (
-    <div className={styles.test}>
-      <div>
-        <ul>
-          {week.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </div>
+    <div className={styles.container}>
+      <table>
+        <thead>
+          <tr>
+            <th>Horários</th>
+            {week.map((day, index) => (
+              <th
+                key={index}
+                style={{ backgroundColor: today === index ? color : '#FFF' }}
+              >
+                {day}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {allNotes.map((item: any, index: number) => {
+            return (
+              <tr key={index}>
+                <td rowSpan={item.numberOfColums}>{`${item.time.getHours()}:${
+                  (item.time.getMinutes() < 10 ? '0' : '') +
+                  item.time.getMinutes()
+                }`}</td>
+                {week.map((_, noteIndex: number) => {
+                  var testNumber = item.notes.filter(
+                    (n: Notes) => n.day === noteIndex
+                  )
+
+                  if (testNumber.length === 1) {
+                    return <td key={noteIndex}>{testNumber[0].name}</td>
+                  } else if (testNumber.length >= 2) {
+                    return (
+                      <td key={noteIndex}>
+                        <table>
+                          <tbody>
+                            {testNumber.map((z: any, i: any) => (
+                              <tr key={i}>
+                                <td>{z.name}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </td>
+                    )
+                  } else {
+                    return <td key={noteIndex}>{null}</td>
+                  }
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
